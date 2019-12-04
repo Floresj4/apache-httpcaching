61cbdab446823fb0599b3fbf51d1d3b244f84bcf
Jason <floresj4@gmail.com>
2019-12-04 18:43:08 -0500

    add cache-control headers

--- httpcaching-service/src/main/java/org/httpcaching/service/SimpleController.java
+++ httpcaching-service/src/main/java/org/httpcaching/service/SimpleController.java
@@ -2,10 +2,12 @@ package org.httpcaching.service;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,12 @@ public class SimpleController {
 
 	@GetMapping(value = "/some/resource", produces = "application/octet-stream")
 	public InputStreamResource getSomeResource(HttpServletResponse response) throws FileNotFoundException {
+		//set the response content type and important headers
 		response.setContentType("application/jpeg");
-		response.setHeader("Content-Disposition", "attachment; filename=\"java.jpg\"");
-		
-		InputStreamResource resource = new InputStreamResource(
-				new FileInputStream("./src/main/resources/java.jpg"));
+		response.addHeader("Content-Disposition", "attachment; filename=\"java.jpg\"");
+		response.addHeader("Cache-Control", CacheControl.maxAge(10, TimeUnit.MINUTES)
+				.getHeaderValue());
 
-		return resource;
+		return new InputStreamResource(new FileInputStream("./src/main/resources/java.jpg"));
 	}
 }
