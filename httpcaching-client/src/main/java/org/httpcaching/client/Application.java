7075426c5e58615afe0b23a51b70458489ebad2f
Jason <floresj4@gmail.com>
2019-12-04 18:27:43 -0500

    adding service url

--- httpcaching-client/src/main/java/org/httpcaching/client/Application.java
+++ httpcaching-client/src/main/java/org/httpcaching/client/Application.java
@@ -2,6 +2,6 @@ package org.httpcaching.client;
 
 public class Application {
 	public static void main(String[] args) {
-
+		final String serviceUrl = "http://localhost:8080/get/some/resource";
 	}
 }
